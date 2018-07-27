b0f0111bf85b145d59e24d200d165d638e2f8622
wal-shahzaib <theshahzaib5@gmail.com>
2018-07-27 18:07:35 +0500

    moved array to state of the component

--- src/components/LearningArrays/index.jsx
+++ src/components/LearningArrays/index.jsx
@@ -3,19 +3,25 @@ import '../../App.css'
 import _ from 'underscore'
 
 class LearningArrays extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      arrayToLoop: ['Welcome! ', 'here ', 'is ', 'my ', 'first ', 'array ', 'Loop']
+    }
+  }
+
   consoleLog = () => {
     console.log('Print in console on onClick')
   }
 
   render() {
-    const arrayToLoop = ['Welcome! ', 'here ', 'is ', 'my ', 'first ', 'array ', 'Loop']
-
     return (
       <div onClick={this.consoleLog}>
         <p>here is my first separate component</p>
 
         <div>
-          { _.map(arrayToLoop, (word, i) => {
+          { _.map(this.state.arrayToLoop, (word, i) => {
               return (
                 <span key={i}>{word}</span>
               )
