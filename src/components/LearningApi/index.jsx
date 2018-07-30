5c43a30ad63156912ee5d3cfec7527de84677f0a
Shah Zaib <theshahzaib5@gmail.com>
2018-07-30 15:05:46 +0500

    bootstrap 4 added to the project

---
+++ src/components/LearningApi/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import '../../App.css'
+
+class LearningApi extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        asdfas
+      </div>
+    )
+  }
+}
+
+export default LearningApi
