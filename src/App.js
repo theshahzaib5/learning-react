2ff701aa00d037e420da0ef2d8410819b3c447c2
Shah Zaib <theshahzaib5@gmail.com>
2018-07-31 19:57:32 +0500

    learning api how to call and print

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
-import Select from 'react-select'
 
 import LearningArrays from './components/LearningArrays'
 import LearningProps from './components/LearningProps'
 import HeaderNav from './components/HeaderNav'
+import LearningApi from './components/LearningApi'
 
 import './App.css'
-
-import {
-  Container,
-  FormGroup,
-  Label,
-} from 'reactstrap'
-
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
-]
 
 class App extends Component {
   constructor(props) {
@@ -28,7 +16,6 @@ class App extends Component {
       toggleButton: true,
       userName: 'Shah Zaib',
       profession: 'Front end developer',
-      selectedOption: null,
     }
   }
 
@@ -44,34 +31,19 @@ class App extends Component {
     })
   }
 
-  handleChange = (selectedOption) => {
-    this.setState({ selectedOption: selectedOption ? selectedOption : '' })
-  }
-
   render() {
-    const {toggleButton, userName, profession, selectedOption} = this.state
+    const {toggleButton, userName, profession} = this.state
 
     return (
       <div>
         <HeaderNav />
-
-        <Container>
-          <FormGroup>
-            <Label>Choose One</Label>
-            <Select
-              value={selectedOption}
-              onChange={this.handleChange}
-              options={options}
-              isMulti={true}
-            />
-          </FormGroup>
-        </Container>
 
         <div className="App">
           <header className="App-header">
             <Welcome toggleButton={this.state.toggleButton} text="Welcom to using props, Welcome to React"/>
             <img src={logo} className="App-logo" alt="logo" />
           </header>
+
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
@@ -89,6 +61,8 @@ class App extends Component {
           <LearningArrays style={{marginBottom: 15,}} />
 
           <LearningProps style={{marginBottom: 15,}} userName={userName} />
+
+          <LearningApi />
         </div>
       </div>
     )
---
+++ src/components/LearningApi/index.jsx
@@ -0,0 +1,107 @@
+import React, { Component } from 'react'
+import _ from 'underscore'
+import Select from 'react-select'
+
+import {
+  Container,
+  Table,
+  FormGroup,
+  Label,
+  Row,
+  Col,
+} from 'reactstrap'
+
+import '../../App.css'
+
+class LearningApi extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      selectedOption: [],
+      // initializing of json data
+      jsonList: [],
+    }
+  }
+
+  componentDidMount() {
+    // this is for fetching json from a url
+    fetch('http://www.json-generator.com/api/json/get/coOfPEdCGa?indent=2', {
+      method: 'GET'
+    })
+    // this will get the response from json file url
+    .then(response => response.json())
+    // this will print json in console.
+    .then(json => {
+      console.log('json json json', json)
+      this.setState({
+        jsonList: json,
+      })
+    })
+    // if there are any errors .catch will through an error on console and json will not printed there untill the error is removed
+    .catch(error => {
+      console.log('error error error', error)
+    })
+  }
+
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption: selectedOption ? selectedOption : ''})
+  }
+
+  render() {
+    let self = this
+    const { jsonList, selectedOption } = self.state
+    const selectList = _.map(jsonList, (item, i) => { return {value: item.name, label: item.name }})
+
+    return (
+      <Container className="text-left">
+        <Row>
+          <Col sm='4'>
+            <FormGroup>
+              <Label>Choose One</Label>
+              <Select
+                value={selectedOption}
+                onChange={this.handleChange}
+                options={selectList}
+              />
+            </FormGroup>
+          </Col>
+        </Row>
+
+        <Table bordered striped>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Address</th>
+              <th>Company</th>
+              <th>Friends</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {/* Loop to list json items */}
+            { _.map(jsonList, (item, i) => {
+                if(item.name === selectedOption.value) {
+                  return (
+                    <tr key={item.name}>
+                      <td>{i}</td>
+                      <td>{item.name ? item.name : 'N/A'}</td>
+                      <td>{item.age ? item.age : 'N/A'}</td>
+                      <td>{item.address ? item.address : 'N/A'}</td>
+                      <td>{item.company ? item.company : 'N/A'}</td>
+                      <td>{item.friends.name ? item.friends.name : 'N/A'}</td>
+                    </tr>
+                  )
+                }
+              })
+            }
+          </tbody>
+        </Table>
+      </Container>
+    )
+  }
+}
+
+export default LearningApi
